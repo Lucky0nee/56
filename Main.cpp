63b5465387c3ee9ed3cdb4cb3ee4c0617a80d8eb
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-02-18 19:25:03 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -17,40 +17,40 @@ public:
             return true;
 
         if ((Symbol.SpecialSymbol == 'O' || Symbol.SpecialSymbol == 'О') && (this->SpecialSymbol == 'O' || this->SpecialSymbol == 'О'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'A' || Symbol.SpecialSymbol == 'А') && (this->SpecialSymbol == 'A' || this->SpecialSymbol == 'А'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'I' || Symbol.SpecialSymbol == 'І') && (this->SpecialSymbol == 'I' || this->SpecialSymbol == 'І'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'E' || Symbol.SpecialSymbol == 'Е') && (this->SpecialSymbol == 'E' || this->SpecialSymbol == 'Е'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'H' || Symbol.SpecialSymbol == 'Н') && (this->SpecialSymbol == 'H' || this->SpecialSymbol == 'Н'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'C' || Symbol.SpecialSymbol == 'С') && (this->SpecialSymbol == 'C' || this->SpecialSymbol == 'С'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'B' || Symbol.SpecialSymbol == 'В') && (this->SpecialSymbol == 'B' || this->SpecialSymbol == 'В'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'K' || Symbol.SpecialSymbol == 'К') && (this->SpecialSymbol == 'K' || this->SpecialSymbol == 'К'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'P' || Symbol.SpecialSymbol == 'Р') && (this->SpecialSymbol == 'P' || this->SpecialSymbol == 'Р'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'X' || Symbol.SpecialSymbol == 'Х') && (this->SpecialSymbol == 'X' || this->SpecialSymbol == 'Х'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'T' || Symbol.SpecialSymbol == 'Т') && (this->SpecialSymbol == 'T' || this->SpecialSymbol == 'Т'))
-                return true;
+            return true;
 
         if ((Symbol.SpecialSymbol == 'M' || Symbol.SpecialSymbol == 'М') && (this->SpecialSymbol == 'M' || this->SpecialSymbol == 'М'))
-                return true;
+            return true;
 
         return false;
     }
@@ -102,8 +102,15 @@ public:
 };
 
 int main() {
+    system("chcp 1251");
+
     SpecialSymbols symbol1('A');
     SpecialSymbols symbol2('А');
+    char t1, t2;
+    cin >> t1 >> t2;
+
+    symbol1.SetSymbols(t1);
+    symbol2.SetSymbols(t2);
 
     if (symbol1 == symbol2)
         cout << "== true" << "\n";
@@ -122,15 +129,11 @@ int main() {
 Створіть клас для роботи з особливими символами. Можете його так і назвати: ОсобливіСимволи. Ось
 перелік особливих символів { O, A, I, E, H, C, B, K, P, X, T, M }. Їх особливість у тому, що вони притаманні
 українській і англійській абеткам. Є ризик не помітити українську букву в англійському слові і навпаки.
-
-Ваш клас має містити перевантажені оператори ==, !=. 
-
+Ваш клас має містити перевантажені оператори ==, !=.
 Таким чином, щоб символи, які в різних абетках пишуть однаково і їх можна сплутати,
-повертали істинне значення при застосуванні оператора == 
-
+повертали істинне значення при застосуванні оператора ==
 і хибне значення при застосуванні !=. Наприклад буква А з української абетки має дорівнювати букві A з
 англійської.
-
 В головній функції дайте користувачу можливість порівнювати різні символи і видавати результат такого
 порівняння. Якщо символ порівнюють з самим собою, то результат теж має бути істинно.
 */
